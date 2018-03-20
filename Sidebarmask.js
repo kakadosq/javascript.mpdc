8ab87bf1ace55112bb44c2a612779cab4f857275
Kaka dosq <35874289+kakadosq@users.noreply.github.com>
2018-03-20 14:37:40 +0700

    Create Sidebarmask.js

---
+++ Sidebarmask.js
@@ -0,0 +1,8 @@
+function myFunction1() {
+   var element = document.getElementById("side-bar");
+   element.classList.toggle("mystyle");
+   var element = document.getElementById("sidebar-mask");
+   element.classList.toggle("open");
+  var body = document.body;
+  body.classList.toggle("flow");
+}
